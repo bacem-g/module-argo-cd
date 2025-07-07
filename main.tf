dbaf52b519c12e5c1aee64971ab7b67cb46f0f93
Bassem Ghali <bghali@unifonic.com>
2025-07-07 14:56:18 +0300

    fix exec line

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "helm" {
   kubernetes {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
       command     = "aws-iam-authenticator"
       args        = ["token", "-i", var.kubernetes_cluster_name]
